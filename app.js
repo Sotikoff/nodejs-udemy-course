11301ae72cf0db485975467b946c5b2ccf6a6b79
Валерий Сотиков <sotikoff@skbkontur.ru>
2022-03-12 16:44:01 +0300

    Add styling

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 const app = express();
 
+app.use(express.static('./public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).send('<h1>404 Page Not Found');
+  res.status(404).sendFile(path.join(__dirname, './views/notFound.html'));
 });
 
 app.listen(3000);
--- routes/shop.js
+++ routes/shop.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const path = require('path');
 
 const shopRouter = express.Router();
 
 shopRouter.get('/', (req, res, next) => {
-  res.send('<h1>U mama gay</h1>');
+  res.sendFile(path.join(__dirname, '../views/shop.html'));
 });
 
 module.exports = shopRouter;
